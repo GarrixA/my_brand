dc761711f2af3218a9ee23df8cfcdb0aa0199dad
GarrixA <aphrogarrix10@gmail.com>
2024-02-23 15:01:58 +0200

    slide

---
+++ styles/swiper.js
@@ -0,0 +1,17 @@
+
+const blogsContainer = document.querySelector('.blogsContainer');
+const nextBtn = document.querySelector('.nexts');
+const prevBtn = document.querySelector('.preve');
+
+nextBtn.addEventListener('click', () => {
+  const firstBlog = blogsContainer.firstElementChild;
+  blogsContainer.appendChild(firstBlog);
+});
+
+prevBtn.addEventListener('click', () => {
+  const lastBlog = blogsContainer.lastElementChild;
+  blogsContainer.prepend(lastBlog);
+});
+
+
+
