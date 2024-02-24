d137612b1ed12a7f7e51e17722dbca0bd8bf4960
GarrixA <aphrogarrix10@gmail.com>
2024-02-24 19:14:53 +0200

    project tab: list of all projects, add, delete and edit project

--- styles/login.js
+++ styles/login.js
@@ -1,4 +1,5 @@
-function validateForm() {
+function validateForm(e) {
+    e.preventDefault();
     var password = document.getElementById("password").value;
     var passwordError = document.getElementById("passwordError");
     var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{6,}$/;
@@ -7,10 +8,9 @@ function validateForm() {
         passwordError.textContent = "Password must be at least 6 characters long and contain a mix of capital letters, small letters, numbers, and special characters.";
         return false;
     } else {
-        passwordError.textContent = ""; // Clear error message if password is valid
-        alert("Form validated! Redirecting to the dashboard..."); // Alert user about form validation
-        // Redirect to the dashboard or perform any other action
-        window.location.href = "/Admin Panel/mainPanel.html";
+        passwordError.textContent = "";
+        alert("Form validated! Redirecting to the dashboard...");
+        window.location.href = "/AdminPanel/mainPanel.html";
         return true;
     }
 }
