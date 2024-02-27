cc971b216d540a8b9a39847e21a7ffa4e80044f1
GarrixA <aphrogarrix10@gmail.com>
2024-02-27 23:24:16 +0200

    addingBlogs: function to add blogs and read them through local storage

--- AdminPanel/fuctionalities/createBlog.js
+++ AdminPanel/fuctionalities/createBlog.js
@@ -1,16 +1,53 @@
-function createBlog(event) {
+
+let blogs = JSON.parse(localStorage.getItem('obj')) || [
+    { img: '', title: 'Manaki Malek', description: 'What are you trying to say', date: '' }
+];
+console.log(blogs)
+
+function readAll() {
+
+    localStorage.setItem('obj', JSON.stringify(blogs));
+
+    let blogsContainer = document.querySelector('.blogsContainer');
+
+    if (blogsContainer) {
+        let obj = localStorage.getItem('obj');
+
+        let objData = JSON.parse(obj);
+        let elements = '';
+
+        objData.map(item => {
+
+            elements += `<div class="blog">
+            <div class="blogImage">
+                <img src="${item.img}">
+            </div>
+            <h2>${item.title}</h2>
+            <p>${item.description}</p>
+            <a href="/singleBlog.html"> <button>view more</button></a>
+        </div>`;
+        });
+
+        blogsContainer.innerHTML = elements;
+    }
+}
+let blogImage = document.querySelector("#image").value
+function addBlog(event) {
     event.preventDefault();
 
-    let blog = {
-        title: document.getElementById('title').value,
-        date: document.getElementById('date').value,
-        description: document.getElementById('description').value
-    };
+    let blogImage = document.querySelector("#image").value
+    let blogTitle = document.querySelector("#title").value
+    let blogDate = document.querySelector("#date").value
+    let blogDescription = document.querySelector("#description").value
 
-    localStorage.setItem('blog', JSON.stringify(blog));
+    
+    let formData = { img: blogImage, title: blogTitle, description: blogDescription.value, date: blogDate }
+
+    blogs.push(formData);
+
+    localStorage.setItem('obj', JSON.stringify(blogs));
+
+    readAll();
 }
 
-let myBlog = localStorage.getItem('blog');
-
-let blog = JSON.parse(myBlog);
-
+readAll();
